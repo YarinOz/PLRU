de7e0bab0294b1fe8643e2c4cb1e60c104f82d68
MarinaBotvinnik <botvinnik95@gmail.com>
2023-12-19 14:00:50 +0200

    color_edge

--- nx_tree.py
+++ nx_tree.py
@@ -37,7 +37,6 @@ def TOUCH(line):  # check if line is miss or hit
     if line not in valid_inputs:
         print("Invalid line!")
         return
-    #hit = False
     for i in range(8,16):
     # hit
         if line == G.nodes[i]['state']:  
@@ -95,26 +94,79 @@ def PLRU():
             elif state == 1:     # right.right.right H
                 plru = 15
                 return plru
+def color():
+    node = 1
+    if G.nodes[node]['state'] == 0:
+        G[node][node+1]['color'] = 'red'
+        node+=1
+        if G.nodes[node]['state'] == 0:
+            G[node][node+2]['color'] = 'red'
+            node+=2
+            if G.nodes[node]['state'] == 0:
+                G[node][node+4]['color'] = 'red'
+                return
+            elif G.nodes[node]['state'] == 1:
+                G[node][node+5]['color'] = 'red'
+                return
+        elif G.nodes[node]['state'] == 1:
+            G[node][node+3]['color'] = 'red'
+            node+=3
+            if G.nodes[node]['state'] == 0:
+                G[node][node+5]['color'] = 'red'
+                return
+            elif G.nodes[node]['state'] == 1:
+                G[node][node+6]['color'] = 'red'
+                return
+    elif G.nodes[node]['state'] == 1:
+        G[node][node+2]['color'] = 'red'
+        node+=2
+        if G.nodes[node]['state'] == 0:
+            G[node][node+3]['color'] = 'red'
+            node+=3
+            if G.nodes[node]['state'] == 0:
+                G[node][node+6]['color'] = 'red'
+                return
+            elif G.nodes[node]['state'] == 1:
+                G[node][node+7]['color'] = 'red'
+                return
+        elif G.nodes[node]['state'] == 1:
+            G[node][node+4]['color'] = 'red'
+            node+=4
+            if G.nodes[node]['state'] == 0:
+                G[node][node+7]['color'] = 'red'
+                return
+            elif G.nodes[node]['state'] == 1:
+                G[node][node+8]['color'] = 'red'
+                return
+            
+if __name__ == "__main__":
         
-G=nx.Graph()
-G.add_edges_from([(1,2), (1,3), (2,4), (2,5), (3,6), (3,7), (4,8), (4,9),
-                  (5,10), (5,11), (6,12), (6,13), (7,14), (7,15)])
+    G=nx.Graph()
+    G.add_edges_from([(1,2), (1,3), (2,4), (2,5), (3,6), (3,7), (4,8), (4,9),
+                    (5,10), (5,11), (6,12), (6,13), (7,14), (7,15)])
 
-PLRU_bits = [1,2,3,4,5,6,7]          # 1, 2, 3, 4, 5, 6, 7 state nodes
-lines = [8,9,10,11,12,13,14,15] # A, B, C, D, E, F, G, H
-valid_inputs = ['A','B','C','D','E','F','G','H','X','Y','Z']
+    PLRU_bits = [1,2,3,4,5,6,7]          # 1, 2, 3, 4, 5, 6, 7 state nodes
+    lines = [8,9,10,11,12,13,14,15] # A, B, C, D, E, F, G, H
+    valid_inputs = ['A','B','C','D','E','F','G','H','X','Y','Z']
 
-for i in PLRU_bits:
-    G.nodes[i]['state']=0
-for i,j in zip(lines,['A','B','C','D','E','F','G','H']):
-    G.nodes[i]['state']=j
+    # state assignment
+    for i in PLRU_bits:
+        G.nodes[i]['state']=0
+    for i,j in zip(lines,['A','B','C','D','E','F','G','H']):
+        G.nodes[i]['state']=j
+    
+    ##### Run touch sequence in format {line1 line2 line3 ...}#####  touch only lines in valid_inputs
+    print("Choose a line to touch:")
 
-##### Run touch sequence #####  touch only lines in valid_inputs
-print("Choose a line to touch:")
-line = TOUCH(input())
+    line_seq = input().split(' ')
+    for line in line_seq:
+        TOUCH(line)
+    # color edges
+    color()
+    edge_colors = [G[u][v]['color'] if 'color' in G[u][v] else 'black' for u, v in G.edges()]
 
+    node_states = {i: G.nodes[i]['state'] for i in G.nodes}
+    pos = hierarchy_pos(G,1)
+    nx.draw(G, pos=pos, labels=node_states,font_size=22,with_labels=True, node_size=1200, edge_color=edge_colors)
+    plt.savefig('PLRU_DT.png')
 
-node_states = {i: G.nodes[i]['state'] for i in G.nodes}
-pos = hierarchy_pos(G,1)
-nx.draw(G, pos=pos, labels=node_states,font_size=22,with_labels=True, node_size=1200)
-plt.savefig('PLRU_DT.png')
