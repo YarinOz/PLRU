eae9d82abbbe7fabb4f3791801f153e9eca02b45
YarinOz <oziely@post.bgu.ac.il>
2024-06-24 12:22:54 +0300

    update

--- abuse.sh
+++ abuse.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 iterable="A E C B G F D B H A E B C G F B D H A B E C G B F D H B "
-input="A B D H X B F H B "
+# input="A B D H X B F H B " # optional if we need to load 4 lines without changing cache
+input="X B F H B "
 for i in {1..1000}; do
     input+=${iterable}
 done
